09929db21772f518c951192157d834d6a004a3b5
brynjar-reynisson <breynisson@gmail.com>
2026-03-29 15:42:17 +0000

    fix: add copyright and disclaimer section to Grand Staff page

--- frontend/src/pages/GrandStaff.tsx
+++ frontend/src/pages/GrandStaff.tsx
@@ -60,9 +60,10 @@ export default function GrandStaff() {
         <li><span className="text-gray-500">Algorithm based on:</span> Chordback JSFX script by Paul Heams</li>
       </ul>
 
-      <p className="text-xs text-gray-400">
-        Copyright © Brynjar Reynisson · breynisson@gmail.com
-      </p>
+      <div className="text-xs text-gray-400 space-y-2">
+        <p>Copyright (C) Brynjar Reynisson</p>
+        <p>THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.</p>
+      </div>
     </div>
   )
 }
