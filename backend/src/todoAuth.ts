1637c5bf28f18181c7d11adf942fce4c821d56e8
brynjar-reynisson <breynisson@gmail.com>
2026-04-10 10:11:45 +0000

    feat: implement verifyPassword with argon2id

--- backend/src/todoAuth.ts
+++ backend/src/todoAuth.ts
@@ -1,13 +1,31 @@
 import fs from 'fs/promises'
 import path from 'path'
 import crypto from 'crypto'
+import * as argon2 from 'argon2';
+
+/**
+ * OWASP 2024 Recommended Minimums:
+ * - memoryCost: 19456 (19 MiB)
+ * - timeCost: 2 iterations
+ * - parallelism: 1
+ */
+const hashOptions: argon2.Options = {
+  type: argon2.argon2id, // Hybrid mode resistant to GPU/side-channel attacks
+  memoryCost: 2 ** 16,   // 64 MiB
+  timeCost: 3,           // 3 iterations
+  parallelism: 4         // Number of threads
+};
 
 function getSessionPath(): string {
   return path.join(process.env.TODO_DIR ?? './TODO', '.session')
 }
 
 export async function verifyPassword(_password: string): Promise<boolean> {
-  return false
+  const hashed = process.env.HASHED_PASSWORD
+  if (!hashed) {
+    return false
+  }
+  return await argon2.verify(hashed, _password);
 }
 
 export function generateToken(): string {
